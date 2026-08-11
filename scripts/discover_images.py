93c7b94dfbdb625e35500659eb43f11765d21169
Jens <j.krumsieck@outlook.de>
2026-08-11 08:48:11 +0000

    added new script

---
+++ scripts/discover_images.py
@@ -0,0 +1,31 @@
+import requests
+
+from argparse import ArgumentParser
+
+parser = ArgumentParser("discover_images")
+parser.add_argument("--namespaces", nargs="+", default=[])
+args = parser.parse_args()
+
+
+def read_page(url):
+    res = requests.get(url)
+    json = res.json()
+
+    results = json["results"]
+
+    if json["next"]:
+        new_results = read_page(json["next"])
+        results += new_results
+
+    return results
+
+
+images = []
+
+for namespace in args.namespaces:
+    endpoint = f"https://hub.docker.com/v2/repositories/{namespace}/?page_size=100"
+    results = read_page(endpoint)
+    names = [f"{r['namespace']}/{r['name']}" for r in results if r["status"] == 1]
+    images += names
+
+print(images)
